a04224289d407f811a881576640438bfc693e925
tildaengberg <tildaengberg@hotmail.com>
2024-01-07 18:09:01 +0100

    fix: sound SFX

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -5,17 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    private AudioManager audioManager;
-
-    private void Awake(){
-        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
-    }
 
     private void OnTriggerEnter(Collider collision){
         if(collision.tag == "Player"){
             SceneManager.LoadScene("FinishMenu");
-            audioManager.PlaySFX(audioManager.finish);
-            Debug.Log("Finish");
         }
     }
 }
